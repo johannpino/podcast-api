c0b78f35eafc5c1a4cace12b1ffdb75a2c576917
Johann Pino <johannpino.dev@gmail.com>
2019-08-12 17:50:17 -0400

    Agregamos manejadores de errores en nextjs

--- pages/channel.js
+++ pages/channel.js
@@ -1,37 +1,47 @@
 import 'isomorphic-fetch'
-import Link from 'next/link'
 import Layout from '../components/Layout'
 import PodcastList from '../components/PodcastList'
 import SeriesList from '../components/SeriesList'
+import Error from './_error';
 
 
 export default class channel extends React.Component {
-    static async getInitialProps({query}){
+    static async getInitialProps({query, res}){
         let idChannel = query.id
+        try{
+            let [ reqChannel, reqAudios, reqSeries ] = await Promise.all([
+                fetch(`https://api.audioboom.com/channels/${idChannel}`),
+                fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips.mp3`),
+                fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`)
+            ])
 
-        let [ reqChannel, reqAudios, reqSeries ] = await Promise.all([
-            fetch(`https://api.audioboom.com/channels/${idChannel}`),
-            fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips.mp3`),
-            fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`)
-        ])
+            if(reqChannel.status >= 400){
+                res.statusCode = reqChannel.status
+                return {channel: null, audioClips: null, series: null, statusCode: reqChannel.status}
+            }
+    
+            let dataChannel = await reqChannel.json()
+            let channel = dataChannel.body.channel
+    
+            let dataAudios = await reqAudios.json()
+            let audioClips = dataAudios.body.audio_clips
+    
+            let dataSeries = await reqSeries.json()
+            let series = dataSeries.body.channels
+    
+            return {channel, audioClips, series, statusCode: 200}
+        }catch(e){
+            return {channel: null, audioClips: null, series: null, statusCode: 503}
+        }
 
-        let dataChannel = await reqChannel.json()
-        let channel = dataChannel.body.channel
-
-        let dataAudios = await reqAudios.json()
-        let audioClips = dataAudios.body.audio_clips
-
-        let dataSeries = await reqSeries.json()
-        let series = dataSeries.body.channels
-
-
-        return {channel, audioClips, series}
     }
 
 
     render() {
-        
-        const {channel,audioClips, series} = this.props
+        const {channel,audioClips, series, statusCode} = this.props
+            if(statusCode !== 200){
+                return <Error statusCode={statusCode} />
+            }
         return (
             <>
                 <Layout title={channel.title}>
